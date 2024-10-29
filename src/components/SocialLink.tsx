00c1d3f121e61907e7c65b3da0969d99c0da03f7
tiagopaes <tiagopaes37@gmail.com>
2024-10-29 14:19:09 -0300

    New version using astro.build

--- src/components/SocialLink.tsx
+++
@@ -1,8 +0,0 @@
-export function SocialLink({ icon: Icon, ...props }: any) {
-  return (
-    <a className="group flex text-sm font-medium transition text-zinc-200 hover:text-primary-500" {...props} title={props['aria-label']}>
-      <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-primary-500" />
-      <span className="ml-4">{props['aria-label']}</span>
-    </a>
-  )
-}
